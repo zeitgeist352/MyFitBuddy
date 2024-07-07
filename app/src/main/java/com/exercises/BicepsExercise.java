c73fb0366f28922443765435765e8d10ba80c2c0
Mehmet Dizdar <146279016+mehmetdzdr@users.noreply.github.com>
2024-07-07 14:32:48 +0300

    exercise classes

--- app/src/main/java/com/exercises/BicepsExercise.java
+++ app/src/main/java/com/exercises/BicepsExercise.java
@@ -1,3 +1,4 @@
+
 import java.io.Serializable;
 
 public class BicepsExercises extends Exercises implements Serializable {
---
+++ app/src/main/java/com/exercises/ChestExercise.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+public class ChestExercise extends Exercises implements Serializable {
+
+    public ChestExercise(double difficulty, String name)
+    {
+        super(difficulty, name);
+    }
+    
+}
