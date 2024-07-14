04ef5a2f1b4a15ae005508cb14c20f867e23e3b4
Onur Ege Yakar <onurege20@hotmail.com>
2024-07-14 16:41:40 +0300

    nutrientlist and nutrient updt

--- app/src/main/java/com/nutrition/Nutrient.java
+++ app/src/main/java/com/nutrition/Nutrient.java
@@ -8,6 +8,10 @@ public class Nutrient {
     private int fat;
     private int grams;
 
+    public Nutrient() {
+        // Default constructor required for calls to DataSnapshot.getValue(Nutrient.class)
+    }
+
     public Nutrient(String name, int calories, int protein, int carbs, int fat, int grams) {
         this.name = name;
         this.calories = calories;
@@ -17,26 +21,23 @@ public class Nutrient {
         this.grams = grams;
     }
 
-    public String getName() {
-        return name;
-    }
+    // Getters and setters
+    public String getName() { return name; }
+    public int getCalories() { return calories; }
+    public int getProtein() { return protein; }
+    public int getCarbs() { return carbs; }
+    public int getFat() { return fat; }
+    public int getGrams() { return grams; }
 
-    public int getCalories() {
-        return calories * grams / 100;
-    }
+    public void setName(String name) { this.name = name; }
+    public void setCalories(int calories) { this.calories = calories; }
+    public void setProtein(int protein) { this.protein = protein; }
+    public void setCarbs(int carbs) { this.carbs = carbs; }
+    public void setFat(int fat) { this.fat = fat; }
+    public void setGrams(int grams) { this.grams = grams; }
 
-    public int getProtein() {
-        return protein;
-    }
-
-    public int getCarbs() {
-        return carbs;
-    }
-
-    public int getFat() {
-        return fat;
-    }
-    public int getGrams(){
-        return grams;
+    @Override
+    public String toString() {
+        return name + ": " + calories + " kcal, " + protein + " g protein, " + carbs + " g carbs, " + fat + " g fat, " + grams + " g";
     }
 }
--- app/src/main/java/com/nutrition/NutrientList.java
+++ app/src/main/java/com/nutrition/NutrientList.java
@@ -2,98 +2,78 @@ package com.nutrition;
 
 import java.util.ArrayList;
 
-public class NutrientList{
+public class NutrientList {
 
     private ArrayList<Nutrient> nutrients;
     private double totalProteins;
     private double totalCarbs;
     private double totalFat;
     private double totalCalories;
-    private ArrayList<Nutrient> nutritionList;
 
-    public NutrientList(ArrayList<Nutrient> nutrients)
-    {
+    public NutrientList(ArrayList<Nutrient> nutrients) {
         this.nutrients = nutrients;
     }
 
-    //getters
-    public double getTotalProteins()
-    {
+    // Getters
+    public ArrayList<Nutrient> getNutrients() {
+        return nutrients;
+    }
+
+    public double getTotalProteins() {
         double totalProtein = 0;
-        for(int i = 0; i < this.nutrients.size(); i++)
-        {
+        for (int i = 0; i < this.nutrients.size(); i++) {
             double proteinAmount = this.nutrients.get(i).getProtein();
             double grams = this.nutrients.get(i).getGrams();
-
             totalProtein += proteinAmount * grams / 100;
         }
-
         return totalProtein;
     }
 
-    public double getTotalCarbs()
-    {
+    public double getTotalCarbs() {
         double totalCarbs = 0;
-        for(int i = 0; i < this.nutrients.size(); i++)
-        {
-            double CarbsAmount = this.nutrients.get(i).getCarbs();
+        for (int i = 0; i < this.nutrients.size(); i++) {
+            double carbsAmount = this.nutrients.get(i).getCarbs();
             double grams = this.nutrients.get(i).getGrams();
-
-            totalCarbs += CarbsAmount * grams / 100;
+            totalCarbs += carbsAmount * grams / 100;
         }
-
         return totalCarbs;
     }
 
-    public double getTotalFats()
-    {
+    public double getTotalFats() {
         double totalFats = 0;
-        for(int i = 0; i < this.nutrients.size(); i++)
-        {
+        for (int i = 0; i < this.nutrients.size(); i++) {
             double fatAmount = this.nutrients.get(i).getFat();
             double grams = this.nutrients.get(i).getGrams();
-
             totalFats += fatAmount * grams / 100;
         }
-
         return totalFats;
     }
 
-    public double getTotalCalories()
-    {
+    public double getTotalCalories() {
         double totalCalories = 0;
-        for(int i = 0; i < this.nutrients.size(); i++)
-        {
+        for (int i = 0; i < this.nutrients.size(); i++) {
             double calories = this.nutrients.get(i).getCalories();
             double grams = this.nutrients.get(i).getGrams();
-
             totalCalories += calories * grams / 100;
         }
-
         return totalCalories;
     }
 
-    //additions
-    public void addNutrient(Nutrient nutrient)
-    {
-        this.nutritionList.add(nutrient);
+    // Additions
+    public void addNutrient(Nutrient nutrient) {
+        this.nutrients.add(nutrient);
     }
 
-    public void removeNutrient(Nutrient nutrient)
-    {
-        this.nutritionList.remove(nutrient);
+    public void removeNutrient(Nutrient nutrient) {
+        this.nutrients.remove(nutrient);
     }
 
-    public String toString()
-    {
-        String nutritionList = "";
-
-        for(int i = 0; i < this.nutrients.size();  i++)
-        {
-            nutritionList = this.nutrients.get(i).toString() + "\n";
+    @Override
+    public String toString() {
+        StringBuilder nutritionList = new StringBuilder();
+        for (int i = 0; i < this.nutrients.size(); i++) {
+            nutritionList.append(this.nutrients.get(i).toString()).append("\n");
         }
-
-        return nutritionList;
+        return nutritionList.toString();
     }
-
 }
